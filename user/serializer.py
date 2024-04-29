9b44367fd19fb1dafaa86cf5e83d7f12cd078d97
Ojo Oluwatobi <ojot630@gmail.com>
2024-04-29 08:37:35 +0100

    revise django and added security

--- user/serializer.py
+++ user/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import BlogUser
 
 
-class UserSerializer(serializers.BaseSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogUser
         fields = ['username', 'email', 'password', 'date_of_birth']
